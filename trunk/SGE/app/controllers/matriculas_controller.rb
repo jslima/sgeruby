470bdeca0b7ad9f4b11b39fc52510b9d866f4409
barbafh <barbafh@51829365-c065-9210-524f-c8655efa7e6d>
2010-07-20 00:19:09 +0000

    Alterada matricula, mas com erros

--- trunk/SGE/app/controllers/matriculas_controller.rb
+++ trunk/SGE/app/controllers/matriculas_controller.rb
@@ -11,21 +11,28 @@ class MatriculasController < ApplicationController
   def matricular
     @matricula = Matricula.new
     @aluno = Aluno.find(params[:id])
-    @matricula.aluno_id = @aluno.id
+    session[:aluno_id] = params[:id]    
   end
 
   def create
     @matricula = Matricula.new(params[:matricula])
-
+    data = Time.new
+    matricula = String(data.year) + String(@matricula.turma.curso_id) + String(@matricula.aluno_id)
+    session[:matricula] = matricula
     respond_to do |format|
-      data = DateTime.new
-      @matricula.matricula = data.year + '' + @matricula.turma.curso_id + '' + @matricula.aluno_id
-      if @matricula.save
-        format.html { redirect_to(consultar_matricula_path(@matricula), :notice => 'A matrícula foi realizada com sucesso.') }
-        format.xml  { render :xml => @matricula, :status => :created, :location => @matricula }
+      session[:turma_id] = @matricula.turma_id
+      if esta_matriculado?
+        flash[:notice] = "O aluno ja esta amtriculado neste curso"
+        redirect_to(matricular_aluno_path(session[:aluno_id]))
       else
-        format.html { render :action => "index" }
-        format.xml  { render :xml => @matricula.errors, :status => :unprocessable_entity }
+        @matricula.matricula = session[:matricula]
+        if @matricula.save
+          format.html { redirect_to(consultar_matricula_path(@matricula), :notice => 'A matrícula foi realizada com sucesso.') }
+          format.xml  { render :xml => @matricula, :status => :created, :location => @matricula }
+        else
+          format.html { render :action => "index" }
+          format.xml  { render :xml => @matricula.errors, :status => :unprocessable_entity }
+        end
       end
     end
   end
@@ -41,6 +48,20 @@ class MatriculasController < ApplicationController
         format.html { render :action => "editar" }
         format.xml  { render :xml => @matricula.errors, :status => :unprocessable_entity }
       end
+    end
+  end
+
+  protected
+  def esta_matriculado?
+    @turma = Turma.find(session[:turma_id])
+    sql = "select a.nome, c.nome from matriculas m, cursos c, turmas t, alunos a
+          where m.aluno_id = a.id AND m.turma_id = t.id AND t.curso_id = c.id AND
+          c.id = #{@turma.curso_id} AND a.id = #{session[:aluno_id]}"
+    @resultado = Matricula.find_by_sql(sql)
+    if @resultado.empty?
+      return false
+    else
+      return true
     end
   end
 
